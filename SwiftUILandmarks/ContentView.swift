806ef3baad5ec6ca1db2822152cceefcb736ef5c
PageKallop <PageMarie78@gmail.com>
2021-01-14 11:57:32 -0600

    Finished Landmark app

--- SwiftUILandmarks/ContentView.swift
+++ SwiftUILandmarks/ContentView.swift
@@ -10,20 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(alignment: .leading){
-            Text("Turtle Rock")
-            .font(.title)
-            HStack {
-            Text("Joshua Tree National Park")
-                .font(.subheadline)
-                .foregroundColor(Color.gray)
-                Spacer()
-            Text("California")
-                Spacer()
-                .font(.subheadline)
-            }
-                
-        }.padding()
+        VStack {
+            
+            MapView().frame(height: 300).edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading){
+                Text("Turtle Rock")
+                .font(.title)
+                HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+                    Spacer()
+                Text("California")
+                    Spacer()
+                    .font(.subheadline)
+                }
+                    
+            }.padding()
+            Spacer()
+        }
        
     }
 }
