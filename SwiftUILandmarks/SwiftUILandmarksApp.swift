60b8bad9bcb875003b88e803b7f74534234a7827
PageKallop <PageMarie78@gmail.com>
2021-01-14 10:41:19 -0600

    Initial Commit

---
+++ SwiftUILandmarks/SwiftUILandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarksApp.swift
+//  SwiftUILandmarks
+//
+//  Created by Page Kallop on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
